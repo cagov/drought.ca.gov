a1f6aae86c575e8cab481f8ee23ca503a1918f51
chachasikes <149436+chachasikes@users.noreply.github.com>
2021-07-05 17:51:17 -0700

    add assets folder; redoing string replacement for media assets

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,32 @@
 const fs = require('fs');
 const CleanCSS = require("clean-css");
 
+const wordpressEditor = "https://live-drought-ca-gov.pantheonsite.io";
+const wordpressEditorApi = "https://live-drought-ca-gov.pantheonsite.io";
+const wordpressEditorMediaFiles = "https://live-drought-ca-gov.pantheonsite.io";
+
+const replacementPaths = {
+  // api: {
+  //   src: "https://live-drought-ca-gov.pantheonsite.io/wp-json/wp/v2/",
+  //   target: "/api/",
+  // }, // Still thinking about this
+  media: {
+    src: "https://live-drought-ca-gov.pantheonsite.io/wp-content/uploads/",
+    target: "/media/"
+  },
+};
+
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
   });
+
+  const replaceContent = (item,searchValue,replaceValue) => {
+    item.template.frontMatter.content = item.template.frontMatter.content
+      .replace(searchValue,replaceValue);
+
+  }
   
   eleventyConfig.addCollection("manualcontent", function(collection) {
     let output = [];
@@ -13,6 +34,11 @@ module.exports = function(eleventyConfig) {
       if(item.data.wordpress.dataset) {
         item.data.title = item.data.wordpress.dataset.data.title;
         item.data.templatestring = item.data.wordpress.dataset.data.template;
+
+        // let apiString = new RegExp('\\' + replacementPaths.api.src, 'gi');
+        let mediaString = new RegExp('\\' + replacementPaths.media.src, 'gi');
+        replaceContent(item,mediaString,replacementPaths.media.target);
+
       }
       output.push(item);
     });
@@ -20,6 +46,9 @@ module.exports = function(eleventyConfig) {
     return output;
   });
 
+
+
+ 
   eleventyConfig.addPassthroughCopy({ "wordpress/media": "media" });
 
   return {
---
+++ src/components/base-css/src/icons/doc/README.md
@@ -0,0 +1,3 @@
+# We need icons
+* copied CA Web font into this folder
+* needs to be rigged up
