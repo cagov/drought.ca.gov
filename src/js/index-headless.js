2ccf7f1a68a766a7306a569eb974a61102a2ca25
Konstantin Koryaka <konstantin@konstantinsimac.attlocal.net>
2021-09-29 14:10:49 -0700

    updated drought to the latest design system components

--- src/js/index-headless.js
+++ src/js/index-headless.js
@@ -4,9 +4,13 @@ import '@cagov/ds-minus';
 import '@cagov/ds-pagination';
 import '@cagov/ds-plus';
 import '@cagov/ds-dropdown-menu';
+import '@cagov/ds-pdf-icon/src/index.js';
+import '@cagov/ds-back-to-top/src/index.js';
+import '@cagov/ds-statewide-header/src/index.js';
+import '@cagov/ds-content-navigation';
 
 // import '../components/menu/index.js';
-import '../components/content-navigation/index.js';
+// import '../components/content-navigation/index.js';
 import '../components/post-list/index.js';
 import '../components/page-alert/index.js';
 import '../components/drought-map/index.js';
