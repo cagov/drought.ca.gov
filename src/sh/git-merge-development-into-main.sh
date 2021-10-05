333b95da195b551b8a45087c74a8ab16d14c85b9
Jon Jensen <github@id.xjensen.com>
2021-10-05 13:31:53 -0700

    Clean up and npm scripts

--- src/sh/git-merge-development-into-main.sh
+++ src/sh/git-merge-development-into-main.sh
@@ -4,6 +4,10 @@ git checkout development
 git pull
 git checkout main
 git pull
-git merge --no-ff --no-commit main
-git restore --source=HEAD --staged --worktree -- wordpress/posts wordpress/pages wordpress/media wordpress/config/wordpress-to-github.development.config.json
+git merge --no-ff --no-commit development
+git restore --source=HEAD --staged --worktree -- \ 
+    wordpress/posts \ 
+    wordpress/pages \ 
+    wordpress/media \ 
+    wordpress/config/wordpress-to-github.development.config.json
 git commit -m "Selective merge: development into main"
--- src/sh/git-merge-main-into-development.sh
+++ src/sh/git-merge-main-into-development.sh
@@ -5,5 +5,8 @@ git pull
 git checkout development
 git pull
 git merge --no-ff --no-commit main
-git restore --source=HEAD --staged --worktree -- wordpress/posts wordpress/pages wordpress/media
+git restore --source=HEAD --staged --worktree -- \
+    wordpress/posts \
+    wordpress/pages \
+    wordpress/media
 git commit -m "Selective merge: main into development"
