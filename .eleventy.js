b1041cb928a75a93afc0250dd926052ce267749e
Chach Sikes <chachsikes@ChachSikes.local>
2021-10-13 07:26:11 -0700

    improvements to og_meta

--- .eleventy.js
+++ .eleventy.js
@@ -128,14 +128,14 @@ module.exports = function(eleventyConfig) {
   
   eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
     // Eleventy 1.0+: use this.inputPath and this.outputPath instead
-    if( outputPath && outputPath.endsWith(".html") ) {
-      let minified = htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true
-      });
-      return minified;
-    }
+    // if( outputPath && outputPath.endsWith(".html") ) {
+    //   let minified = htmlmin.minify(content, {
+    //     useShortDoctype: true,
+    //     removeComments: true,
+    //     collapseWhitespace: true
+    //   });
+    //   return minified;
+    // }
 
     return content;
   });
