6a6addd6ddc78f66634e64199f3c7261da2acea4
Chach Sikes <chachsikes@ChachSikes.local>
2021-10-16 09:43:27 -0700

    update staging merge script

--- src/sh/git-merge-main-into-staging.sh
+++ src/sh/git-merge-main-into-staging.sh
@@ -5,19 +5,19 @@ git pull
 git checkout staging
 git pull
 git merge --no-ff --no-commit main
+# Don't merge production content into staging from this script.
+# Don't merge wordpress-to-github settings with branch specific settings
+# Don't merge build settings
+# Don't merge package.json changes because staging is used to test npm package updates
 git restore --source=HEAD --staged --worktree -- \
-    # Don't merge production content into staging from this script.
     wordpress/posts \
     wordpress/pages \
     wordpress/media \
-    # Don't merge wordpress-to-github settings with branch specific settings
     wordpress/wordpress-to-github.config.json \
-    wordpress/wordpress-to-github.main-test-pantheon.config.json
+    wordpress/wordpress-to-github.main-test-pantheon.config.json \
     wordpress/general \
-    # Don't merge build settings
     odi-publishing \
     .github/workflows \
-    # Don't merge package.json changes because staging is used to test npm package updates
     package.json \
-    package-lock.json \
+    package-lock.json
 git commit -m "Selective merge: main into staging"
