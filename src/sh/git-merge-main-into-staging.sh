aa52410121c06fbd864720b5b4d383fef9a299d7
Chach Sikes <chachsikes@ChachSikes.local>
2021-10-16 10:13:47 -0700

    Update staging merge script to retore files and then re-checkout the staging wordpress content to prevent any possible partially staged merges. Comment out the merge commit because there can be merge conflicts that need review.

--- src/sh/git-merge-main-into-staging.sh
+++ src/sh/git-merge-main-into-staging.sh
@@ -10,14 +10,11 @@ git merge --no-ff --no-commit main
 # Don't merge build settings
 # Don't merge package.json changes because staging is used to test npm package updates
 
-# Check out an exact copy of wordpress posts, pages and media from staging
-git checkout staging \
-    wordpress/posts \
-    wordpress/pages \
-    wordpress/media
-
 # Try git restore (@TODO let's write a description of exactly what this is doing for everyone's benefit)
 git restore --source=HEAD --staged --worktree -- \
+    wordpress/posts \
+    wordpress/pages \
+    wordpress/media \
     wordpress/wordpress-to-github.config.json \
     wordpress/wordpress-to-github.main-test-pantheon.config.json \
     wordpress/general \
@@ -25,6 +22,12 @@ git restore --source=HEAD --staged --worktree -- \
     .github/workflows \
     package.json \
     package-lock.json
+
+# Check out an exact copy of wordpress posts, pages and media from staging. This should fully reset any diff changes for content that differs from main and staging.
+git checkout staging \
+    wordpress/posts \
+    wordpress/pages \
+    wordpress/media    
 
 # @TODO Check for any merge conflicts. If no, it may be safe to do a the commit. 
 # We also want to create this as another branch.
