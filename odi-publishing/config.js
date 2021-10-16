15048f016a687bb3a023ecc923d476c8fb2c5169
Chach Sikes <chachsikes@ChachSikes.local>
2021-10-16 10:55:47 -0700

    Add odi-publishing shell scripts and publishing settings

--- odi-publishing/config.js
+++ odi-publishing/config.js
@@ -1,10 +1,10 @@
 const main = require('./odi-publishing.json'); // Default settings.
 const main_test_pantheon = require('./odi-publishing.main-test-pantheon.json'); // Default settings.
-// const staging = require('./odi-publishing.staging.json'); // Default settings.
+const staging = require('./odi-publishing.staging.json'); // Default settings.
 const localhost = require('./odi-publishing.localhost.json'); // Default settings.
 
 const getConfig = () => {
-    var branch = "main"; // @TEMP connect to env varible or github action.
+    var branch = "staging"; // @TEMP connect to env varible or github action.
     // @TODO connect .yml or local env variable branch name & return if branch exists.
     
     switch (branch) {
@@ -14,8 +14,8 @@ const getConfig = () => {
             return main;
         case 'main_test_pantheon': 
             return main_test_pantheon;
-        // case 'staging': 
-        //     return staging;
+        case 'staging': 
+            return staging;
         default:
             return main;
     }
--- src/sh/git-merge-main-into-staging.sh
+++ src/sh/git-merge-main-into-staging.sh
@@ -4,11 +4,15 @@ git checkout main
 git pull
 git checkout staging
 git pull
+echo "Doing a no fast-forward, no commit merge from main into staging branch"
 git merge --no-ff --no-commit main
 # Don't merge production content into staging from this script.
 # Don't merge wordpress-to-github settings with branch specific settings
 # Don't merge build settings
 # Don't merge package.json changes because staging is used to test npm package updates
+
+echo "Try git restore to unset any branch specific settings"
+# (@TODO let's write a description of exactly what this is doing for everyone's benefit)
 git restore --source=HEAD --staged --worktree -- \
     wordpress/posts \
     wordpress/pages \
@@ -20,4 +24,15 @@ git restore --source=HEAD --staged --worktree -- \
     .github/workflows \
     package.json \
     package-lock.json
-git commit -m "Selective merge: main into staging"
+
+echo "Checking out an exact copy of wordpress posts, pages and media from staging. This should fully reset any diff changes for content that differs from main and staging."
+git checkout staging \
+    wordpress/posts \
+    wordpress/pages \
+    wordpress/media    
+
+
+git status
+# @TODO Check for any merge conflicts. If no, it may be safe to do a the commit. 
+# We also want to create this as another branch.
+# git commit -m "Selective merge: main into staging"
