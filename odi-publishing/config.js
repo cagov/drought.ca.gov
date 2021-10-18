8133916a501237815731c34a342d7a2c6aea9189
Chach Sikes <chachsikes@ChachSikes.local>
2021-10-18 12:30:09 -0700

    Change path to use correct branch name

--- odi-publishing/config.js
+++ odi-publishing/config.js
@@ -1,10 +1,27 @@
 const branch = require('git-branch');
-const main = require('./odi-publishing.json'); // Default settings.
-const defaultBranch = require('./odi-publishing.json'); // Default settings.
-const main_test_pantheon = require('./odi-publishing.main-test-pantheon.json'); // Default settings.
+/* Load config files */
+const main = require('./odi-publishing.json'); // `main` branch
+const defaultBranch = require('./odi-publishing.json'); // Default settings (`main`)
+const main_test_pantheon = require('./odi-publishing.main-test-pantheon.json'); // `main-test-pantheon`
 const staging = require('./odi-publishing.staging.json'); // Default settings.
 const development = require('./odi-publishing.development.json'); // Default settings.
 const localhost = require('./odi-publishing.localhost.json'); // Default settings.
+
+/**
+ * Site specific configurations. Load the correct config file for the current branch
+ * SEE ALSO `.github/workflows/*.yml` for links to AWS buckets
+ * SEE ALSO `wordpress/wordpress-to-github.*.json` or `wordpress/config` (future) for content service settings.
+ *
+ * What belongs in the config file:
+ * - Any hard coded strings that need translations
+ * - Path settings and override links for 11ty builds
+ *
+ * Maintenance:
+ * - Information from this file needs to be kept in sync with wordpress/general/general.json - which is using WordPress Editor settings to get the site name. This could get out of sync and break site metadata. WARNING: Don't change the general.json path. It is overwritten by wordpress-to-github with date stamps of when the build ran last, which is injected into all wordpress/pages, posts and media files.
+ * @TODO integration with feature branches (proposed)
+ * 
+ * @returns Object of site specific configurations.
+ */
 
 const getConfig = () => {
 
@@ -18,7 +35,7 @@ const getConfig = () => {
                 return localhost;
             case 'main': 
                 return main;
-            case 'main_test_pantheon': 
+            case 'main-test-pantheon': // Note the dashes.
                 return main_test_pantheon;
             case 'staging': 
                 return staging;
