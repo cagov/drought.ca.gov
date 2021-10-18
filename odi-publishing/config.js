dee916b90b4cb0f95ee2bf76cc5c06e1985d0e43
Chach Sikes <chachsikes@ChachSikes.local>
2021-10-18 12:06:18 -0700

    Try using git-branch plugin to get local branch settings.

--- odi-publishing/config.js
+++ odi-publishing/config.js
@@ -1,24 +1,36 @@
+const branch = require('git-branch');
 const main = require('./odi-publishing.json'); // Default settings.
+const defaultBranch = require('./odi-publishing.json'); // Default settings.
 const main_test_pantheon = require('./odi-publishing.main-test-pantheon.json'); // Default settings.
-// const staging = require('./odi-publishing.staging.json'); // Default settings.
+const staging = require('./odi-publishing.staging.json'); // Default settings.
+const development = require('./odi-publishing.development.json'); // Default settings.
 const localhost = require('./odi-publishing.localhost.json'); // Default settings.
 
 const getConfig = () => {
-    var branch = "main_test_pantheon"; // @TEMP connect to env varible or github action.
-    // @TODO connect .yml or local env variable branch name & return if branch exists.
+
+    var defaultBranch = "main"; // Default to main branch.
+
+    branch('./odi-publishing')
+    .then(name => {
+        console.log('Build is on branch:', name);
+        switch (branch) {
+            case 'localhost': 
+                return localhost;
+            case 'main': 
+                return main;
+            case 'main_test_pantheon': 
+                return main_test_pantheon;
+            case 'staging': 
+                return staging;
+            case 'development': 
+                return development;
+            default:
+                return defaultBranch;
+        }
+    })
+    .catch(console.error, (error) => console.error(error));
     
-    switch (branch) {
-        case 'localhost': 
-            return localhost;
-        case 'main': 
-            return main;
-        case 'main_test_pantheon': 
-            return main_test_pantheon;
-        // case 'staging': 
-        //     return staging;
-        default:
-            return main;
-    }
+    return main; // Return default.
 }
 
 exports.getConfig = getConfig;
