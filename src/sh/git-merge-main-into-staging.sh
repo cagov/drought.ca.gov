f099008860b2d51c17a3d84ca5caa2d314bfd379
Chach Sikes <chachsikes@ChachSikes.local>
2021-10-18 08:26:35 -0700

    Merge more changes from Sync changes and from Friday's 1.1.0 release (post-list) Sync staging build updates to development branch see PR 168 #167

---
+++ src/sh/git-merge-main-into-staging.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+git fetch
+git checkout main
+git pull
+git checkout staging
+git pull
+echo "Doing a no fast-forward, no commit merge from main into staging branch"
+git merge --no-ff --no-commit main
+# Don't merge production content into staging from this script.
+# Don't merge wordpress-to-github settings with branch specific settings
+# Don't merge build settings
+# Don't merge package.json changes because staging is used to test npm package updates
+
+echo "Try git restore to unset any branch specific settings"
+# (@TODO let's write a description of exactly what this is doing for everyone's benefit)
+git restore --source=HEAD --staged --worktree -- \
+    wordpress/posts \
+    wordpress/pages \
+    wordpress/media \
+    wordpress/wordpress-to-github.config.json \
+    wordpress/wordpress-to-github.main-test-pantheon.config.json \
+    wordpress/general \
+    odi-publishing \
+    .github/workflows \
+    package.json \
+    package-lock.json
+
+echo "Checking out an exact copy of wordpress posts, pages and media from staging. This should fully reset any diff changes for content that differs from main and staging."
+git checkout staging \
+    wordpress/posts \
+    wordpress/pages \
+    wordpress/media    
+
+
+git status
+# @TODO Check for any merge conflicts. If no, it may be safe to do a the commit. 
+# We also want to create this as another branch.
+# git commit -m "Selective merge: main into staging"
---
+++ wordpress/general/WARNING.md
@@ -0,0 +1,3 @@
+WARNING! 
+
+Do not change this, wordpress-to-github overwrites this file. If there is an error it can delete a whole folder in the github repo.
