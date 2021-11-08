7fd5c6f6ef653acddbf24cf269ecab25043ec937
Jon Jensen <github@id.xjensen.com>
2021-11-08 11:28:48 -0800

    Beta design-system components, purge dead code

--- src/components/base-css/src/icons/doc/README.md
+++
@@ -1,3 +0,0 @@
-# We need icons
-* copied CA Web font into this folder
-* needs to be rigged up
--- src/js/index-headless.js
+++ src/js/index-headless.js
@@ -4,9 +4,8 @@ import '@cagov/ds-minus';
 import '@cagov/ds-pagination';
 import '@cagov/ds-plus';
 import '@cagov/ds-dropdown-menu';
+import '@cagov/ds-content-navigation';
 
-// import '../components/menu/index.js';
-import '../components/content-navigation/index.js';
 import '../components/post-list/index.js';
 import '../components/page-alert/index.js';
 import '../components/drought-map/index.js';
