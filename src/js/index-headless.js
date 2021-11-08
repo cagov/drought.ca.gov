383eecb6701ee4488c6dad55ba84923f5fb960c1
Jon Jensen <github@id.xjensen.com>
2021-11-08 10:34:21 -0800

    Update design-system modules, purge dead code

--- src/js/index-headless.js
+++ src/js/index-headless.js
@@ -4,9 +4,11 @@ import '@cagov/ds-minus';
 import '@cagov/ds-pagination';
 import '@cagov/ds-plus';
 import '@cagov/ds-dropdown-menu';
+import '@cagov/ds-content-navigation';
+import '@cagov/ds-external-link-icon/src/index.js';
+import '@cagov/ds-pdf-icon/src/index.js';
+import '@cagov/ds-back-to-top/src/index.js';
 
-// import '../components/menu/index.js';
-import '../components/content-navigation/index.js';
 import '../components/post-list/index.js';
 import '../components/page-alert/index.js';
 import '../components/drought-map/index.js';
