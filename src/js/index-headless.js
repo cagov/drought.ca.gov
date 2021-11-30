c4d6985be672e19615a09814277560a5e4ec86e3
Konstantin Koryaka <konstantin@konstantinsimac.attlocal.net>
2021-11-30 11:55:11 -0800

    put external-link-icon JS import after pdf-icon JS to fix #221

--- src/js/index-headless.js
+++ src/js/index-headless.js
@@ -5,8 +5,8 @@ import '@cagov/ds-pagination';
 import '@cagov/ds-plus';
 import '@cagov/ds-dropdown-menu';
 import '@cagov/ds-content-navigation';
-import '@cagov/ds-external-link-icon/src/index.js';
 import '@cagov/ds-pdf-icon/src/index.js';
+import '@cagov/ds-external-link-icon/src/index.js';
 import '@cagov/ds-back-to-top/src/index.js';
 
 import '../components/post-list/index.js';
