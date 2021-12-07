ddd2c50af852ffbcb63735af0e0d5563cc5a1614
Jon Jensen <github@id.xjensen.com>
2021-12-06 16:28:52 -0800

    Revert link-icon in staging

--- src/js/index-headless.js
+++ src/js/index-headless.js
@@ -15,7 +15,8 @@ import '@cagov/ds-external-link-icon/src/index.js';
 import '@cagov/ds-plus';
 import '@cagov/ds-dropdown-menu';
 import '@cagov/ds-content-navigation';
-import '@cagov/ds-link-icon/src/index.js';
+import '@cagov/ds-pdf-icon/src/index.js';
+import '@cagov/ds-external-link-icon/src/index.js';
 import '@cagov/ds-back-to-top/src/index.js';
 
 import '../components/post-list/index.js';
